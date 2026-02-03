d5b227043fde0b6361d54fdf47357f285e30b62e
0309hyejiyang-svg <0309hyejiyang@gmail.com>
2026-02-03 14:43:57 +0900

    Update create.js

--- netlify/functions/create.js
+++ netlify/functions/create.js
@@ -2,19 +2,23 @@ import { neon } from "@netlify/neon";
 
 export const handler = async (event) => {
   const sql = neon();
-  const data = JSON.parse(event.body);
+  const data = JSON.parse(event.body || "{}");
+
+  const {
+    id,
+    name,
+    category,
+    price,
+    status,
+    description,
+    createdAt
+  } = data;
 
   await sql`
     insert into public.products (
       id, name, category, price, status, description, created_at
     ) values (
-      ${crypto.randomUUID()},
-      ${data.name},
-      ${data.category},
-      ${data.price},
-      ${data.status},
-      ${data.description},
-      ${Date.now()}
+      ${id}, ${name}, ${category}, ${price}, ${status}, ${description}, ${createdAt}
     )
   `;
 
